684a06508e4e350303ca9bb34cdd69377e111418
chathurya <149754942+chathuryaadapa@users.noreply.github.com>
2025-04-15 00:14:17 +0530

    Create code-opt.java

---
+++ code-opt.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordCounter {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        words.add("apple");
+        words.add("banana");
+        words.add("apple");
+        words.add("orange");
+        words.add("banana");
+        words.add("banana");
+
+        System.out.println("optimization code");
+    }
+}
